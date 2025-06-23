90cc074ae636469946199773302a530987971e07
Patrick Johann Dwyer <patrickjohanndwyer@gmail.com>
2025-06-22 22:01:44 -0600

    Completed day 18 challenge.

--- day18 - turtle painting/project18.py
+++ day18 - turtle painting/project18.py
@@ -13,15 +13,29 @@ for i in colors:
 
     color_list.append((r, g, b))
 
+#get rid of white dot colors
+color_list = color_list[4:]
 
 timmy = Turtle()
 timmy.shape('turtle')
-timmy.color('black')
+timmy.pu()
+timmy.speed(0)
+timmy.hideturtle()
 
 screen = Screen()
 screen.colormode(255)
 
-timmy.dot(50)
+for i in range(10): 
+
+    for k in range(10):
+        colour = choice(color_list)
+        r,g,b = colour[0], colour[1], colour[2]
+        timmy.fd(50)
+        timmy.dot(20, (r,g,b))
+
+    timmy.setx(0)
+    
+    timmy.sety(timmy.position()[1] + 25)
 
 screen.exitonclick()
 # for row in range(10):
