bf5a1ddd52593aebc26c03b6f5e75b27cd0e2e26
Patrick Johann Dwyer <patrickjohanndwyer@gmail.com>
2025-06-25 17:59:50 -0600

    Completed challenge of making an etch-a-sketch using turtle graphics package!

---
+++ day19 - instances, state and higher order functions/etch-a-sketch.py
@@ -0,0 +1,31 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+def move_forwards():
+    tim.forward(10)
+
+def move_backwards():
+    tim.backward(10)
+
+def move_left():
+    tim.left(15)
+    tim.forward(10)
+
+def move_right():
+    tim.right(15)
+    tim.forward(10)
+
+def clear_center():
+    tim.reset()
+
+screen.listen()
+screen.onkey(key="space", fun=move_forwards)
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=move_left)
+screen.onkey(key="d", fun=move_right)
+screen.onkey(key="c", fun=clear_center)
+
+screen.exitonclick()
